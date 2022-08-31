f4e48287df130a5030b1243bbf11aa0d7dfdea7d
Anthony Sosa <tony96sosa@hotmail.com>
2022-08-31 14:43:24 -0300

    app web working

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({onNewCategory}) => {
 	const [ inputValue, setInputValue ] = useState('');
@@ -22,3 +23,7 @@ export const AddCategory = ({onNewCategory}) => {
 		</form>
 	)
 };
+
+AddCategory.propTypes = {
+	onNewCategory: PropTypes.func.isRequired,
+}
