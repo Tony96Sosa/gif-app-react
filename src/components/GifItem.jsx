0fd0ea106e5eed97f0492d0955d9e1ca57b3be89
Anthony Sosa <tony96sosa@hotmail.com>
2022-08-31 15:55:44 -0300

    testing GifItem component

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,13 +1,15 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export const GifItem = ({title, url}) => (
-    <div className="card">
-        <img src={url} alt={title} />
-        <p>{title}</p>
-    </div>
+const GifItem = ({ title, url, id }) => (
+  <div className="card">
+    <img src={url} alt={title} />
+    <p>{title}</p>
+  </div>
 );
 
 GifItem.propTypes = {
-    title: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
+
+export default GifItem;
---
+++ src/components/GifItem.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import GifItem from "./GifItem";
+
+describe("Prueba en <GifItem/>", () => {
+  const title = "Goku";
+  const url = "https://dragon-ball.com/Goku.jpg";
+
+  test("Deberia matchear con su snapshot", () => {
+    const { container } = render(<GifItem title={title} url={url} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test("Debe de mostrar el alt y src indicados", () => {
+    render(<GifItem title={title} url={url} />);
+    // screen.debug();
+    const { alt, src } = screen.getByRole("img");
+    expect(alt).toBe(title);
+    expect(src).toBe(url);
+  });
+
+  test("Debe de mostrar el titulo en el componente", () => {
+    render(<GifItem title={title} url={url} />);
+    expect(screen.getByText(title)).toBeTruthy();
+  });
+});
